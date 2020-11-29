747f8df58197bdbcf87ede99a450fa6aa24f0688
Mat Warger <mwarger@gmail.com>
2020-11-28 20:03:19 -0600

    updated path

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="typescript">
   import "./utils/TailwindImport.svelte"
-  import Main from "./feature/workout/Main.svelte"
+  import Main from "./feature/main/Main.svelte"
 </script>
 
 <Main />
